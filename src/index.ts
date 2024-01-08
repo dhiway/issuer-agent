cc85c504f01be8702cf181d2e6809799a52fbdf7
vikastc <vikas.tc@dhiway.com>
2024-01-08 16:21:13 +0530

    Initial v3 commit
    Signed-off-by: vikastc <vikas.tc@dhiway.com>

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,101 @@
-import express from "express";
-import bodyParser from "body-parser";
-import fs from "fs";
-import swaggerUi from "swagger-ui-express";
-import cluster from "node:cluster";
-import os from "node:os";
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import cluster from 'node:cluster';
+import os from 'node:os';
 
 import {
-  getCredById,
-  issueCred,
-  revokeCred,
-  updateCred,
-} from "./controller/credential_controller";
-import { createSchema, getSchemaById } from "./controller/schema_controller";
+    getCredById,
+    issueCred,
+    revokeCred,
+    updateCred,
+} from './controller/credential_controller';
+import { createSchema, getSchemaById } from './controller/schema_controller';
 import {
-  createRegistry,
-  getRegistryById,
-} from "./controller/registry_controller";
-import { createConnection } from "typeorm";
-import { dbConfig } from "./dbconfig";
-import { issuerDid, setupDidAndIdentities } from "./init";
+    createRegistry,
+    getRegistryById,
+} from './controller/registry_controller';
+import { createConnection } from 'typeorm';
+import { dbConfig } from './dbconfig';
+import { addDelegateAsRegistryDelegate } from './init';
 
 const app = express();
 export const { PORT } = process.env;
 
-app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(express.json());
 
 const numCPUs = os.cpus().length;
 
 if (cluster.isMaster) {
-  console.log(`Primary ${process.pid} is running`);
+    console.log(`Primary ${process.pid} is running`);
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
-  const credentialRouter = express.Router({ mergeParams: true });
-  const schemaRouter = express.Router({ mergeParams: true });
-  const registryRouter = express.Router({ mergeParams: true });
-
-  credentialRouter.post("/", async (req, res) => {
-    return await issueCred(req, res);
-  });
-  credentialRouter.get("/:id", async (req, res) => {
-    return await getCredById(req, res);
-  });
-  credentialRouter.post("/revoke", async (req, res) => {
-    return await revokeCred(req, res);
-  });
-  credentialRouter.put("/update", async (req, res) => {
-    return await updateCred(req, res);
-  });
-
-  schemaRouter.post("/", async (req, res) => {
-    return await createSchema(req, res);
-  });
-  schemaRouter.get("/:id", async (req, res) => {
-    return await getSchemaById(req, res);
-  });
-
-  registryRouter.post("/", async (req, res) => {
-    return await createRegistry(req, res);
-  });
-  registryRouter.get("/:id", async (req, res) => {
-    return await getRegistryById(req, res);
-  });
-
-  const openApiDocumentation = JSON.parse(
-    fs.readFileSync("./apis.json").toString()
-  );
-
-  app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
-  app.use("/api/v1/cred", credentialRouter);
-  app.use("/api/v1/schema", schemaRouter);
-  app.use("/api/v1/registry", registryRouter);
-
-  async function main() {
-    try {
-      await createConnection(dbConfig);
-    } catch (error) {
-      console.log("error: ", error);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
 
-    app.listen(PORT, () => {
-      console.log(`Dhiway gateway is running at http://localhost:${PORT}`);
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
     });
-    if (!issuerDid) {
-      await setupDidAndIdentities();
-    }
-  }
+} else {
+    const credentialRouter = express.Router({ mergeParams: true });
+    const schemaRouter = express.Router({ mergeParams: true });
+    const registryRouter = express.Router({ mergeParams: true });
 
-  main().catch((e) => console.log(e));
-  console.log(`Worker ${process.pid} started`);
+    credentialRouter.post('/', async (req, res) => {
+        return await issueCred(req, res);
+    });
+    credentialRouter.get('/:id', async (req, res) => {
+        return await getCredById(req, res);
+    });
+    credentialRouter.post('/revoke', async (req, res) => {
+        return await revokeCred(req, res);
+    });
+    credentialRouter.put('/update', async (req, res) => {
+        return await updateCred(req, res);
+    });
+
+    schemaRouter.post('/', async (req, res) => {
+        return await createSchema(req, res);
+    });
+    schemaRouter.get('/:id', async (req, res) => {
+        return await getSchemaById(req, res);
+    });
+
+    registryRouter.post('/', async (req, res) => {
+        return await createRegistry(req, res);
+    });
+    registryRouter.get('/:id', async (req, res) => {
+        return await getRegistryById(req, res);
+    });
+
+    const openApiDocumentation = JSON.parse(
+        fs.readFileSync('./apis.json').toString()
+    );
+
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+    app.use('/api/v1/cred', credentialRouter);
+    app.use('/api/v1/schema', schemaRouter);
+    app.use('/api/v1/registry', registryRouter);
+
+    async function main() {
+        try {
+            await createConnection(dbConfig);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+
+        await addDelegateAsRegistryDelegate();
+
+        app.listen(PORT, () => {
+            console.log(
+                `Dhiway gateway is running at http://localhost:${PORT}`
+            );
+        });
+    }
+
+    main().catch((e) => console.log(e));
+    console.log(`Worker ${process.pid} started`);
 }
