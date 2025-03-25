7139d1acb647242e8ff6124cd52b52e9999dfa3e
Prashant Mutnale <prashant@dhiway.com>
2025-03-25 13:19:36 +0530

    fix:handle origins if the there is a custom middleware
    Signed-off-by: Prashant Mutnale <prashant@dhiway.com>

--- src/server.ts
+++ src/server.ts
@@ -71,6 +71,34 @@ app.use(
   })
 );
 
+// Manually handle OPTIONS requests
+app.options('*', (req, res) => {
+  const origin = req.headers.origin;
+  let tmpOrigin = origin;
+
+  if (origin && origin.slice(-1) === '/') {
+    tmpOrigin = origin.substring(0, origin.length - 1);
+  }
+
+  // Validate origin
+  if (tmpOrigin) {
+    const b = tmpOrigin.split('/')[2].split('.');
+    const domain = `${b[b.length - 2]}.${b[b.length - 1]}`;
+    if (!allowedDomains.includes(domain) && !allowedDomains.includes(tmpOrigin)) {
+      return res.status(403).send('CORS policy: Origin not allowed');
+    }
+  }
+
+  res.set({
+    'Access-Control-Allow-Origin': origin || '*',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH',
+    'Access-Control-Allow-Headers':
+      'Content-Type, X-UserId, Accept, Authorization, user-agent, Host, X-Forwarded-For, Upgrade, Connection, X-Content-Type-Options, Content-Security-Policy, X-Frame-Options, Strict-Transport-Security',
+    'Access-Control-Allow-Credentials': 'true',
+  });
+  res.sendStatus(204); // No content
+});
+
 const openApiDocumentation = YAML.load('./apis.yaml');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
